47534d2ae4f0d8abb88c05039f1930e0d475e270
shahaab-zz <shahaab.asemany@gmail.com>
2025-07-09 11:21:25 +0330

    Proxy rahavard365.com

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+AUTH_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTE4MjE1MDEsImp0aSI6IjVmOTc3ZDA3YWY5ODQ0ODBiY2IzMzBlM2NlZTBjNjM0Iiwic3ViIjoiMTc4MTE4MyIsIm5iZiI6MTc1MTgyMTUwMSwiZXhwIjoxNzU5NTk3NTYxLCJpc3MiOiJjb20ubWFibmFkcC5hcGkucmFoYXZhcmQzNjUudjEifQ.nWrNfmZvFXfjBylDhaDq6yT1Tirdk4yyXXyVUJ7-TnHF2NzRIhRH08trAD82Fm3Mm3rAJOadN1RbeFe05tQIRECe68oyGKgKOS4cst0fRUfDr-AHDZHOPNYY6MPpshe18_vueFoNWkahPpLNxbx7obIMT_elK_2UALMKDxh1BL8mTYSquJoo3xwfscUT55GPi9X0hMxUu_igXcoC-ZoKEDji4nqcYmUZ7UKJ9yreb0hIN_uu5I3KH8hGFOETBx39z7WjK2KwwcFs3J2K-FrefExkd1ynsrxgHbbiaWyNbWil5o7CP13SZ3P9PYjNPZqabGQzMl07wP4V6NbIEPEjDw"
+
+URL = (
+    "https://rahavard365.com/api/v2/chart/bars?"
+    "countback=334&"
+    "symbol=exchange.asset:673:real_close:type0&"
+    "resolution=D&"
+    "from=2022-07-06T00:00:00Z&"
+    "to=2023-10-16T00:00:00Z"
+)
+
+headers = {
+    "Authorization": f"Bearer {AUTH_TOKEN}",
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+    "platform": "web",
+    "application-name": "rahavard"
+}
+
+@app.route('/')
+def home():
+    return "🚀 سرور پروکسی فعال است! برای تست از مسیر /api/nouri استفاده کنید."
+
+@app.route('/api/nouri')
+def nouri():
+    try:
+        response = requests.get(URL, headers=headers, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return jsonify({"status": "✅ موفق", "data": data})
+    except Exception as e:
+        return jsonify({"status": "⛔ خطا در اتصال", "error": str(e)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
